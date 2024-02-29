71aa39107444ccacdba8bd97e56106df13c589d7
Ivan <ivan2022@list.ru>
2024-02-29 16:49:12 +0300

    3 commit

--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
+    title: "BLACK Shrek",
+    subtitle: "Имитация работы казино",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+    image: "./test1.png",
+    link: "https://github.com/ivanP254/BlackShrek",
   },
   {
     title: "React Tracks",
